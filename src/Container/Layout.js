bd53ae7a17a5eb406e11c683f443f1f87dc44bbd
yazan widyan <yazanwidyan1@gmail.com>
2020-03-18 17:54:00 +0200

    adding Layout pages content

---
+++ src/Container/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "./About";
+import Skills from "./Skills";
+import Education from "./Education";
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <About />
+      <Skills />
+      <Education />
+    </div>
+  );
+};
+
+export default Layout;
