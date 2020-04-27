1b8f617a256014139761f789d9b332608be0b992
yazan widyan <yazanwidyan1@gmail.com>
2020-04-27 22:45:18 +0300

    New Portfolio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import ResponsiveDrawer from "./Component/Sidebar";
-import Layout from "./Container/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <ResponsiveDrawer />
-      <Layout />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./Style/App.scss";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
