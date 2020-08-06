f1cf05d9c86845eb08bfce2e0896f43d8e3a6621
Yazan widyan <yazanwidyan1@gmail.com>
2020-08-06 15:08:15 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./Containers/Home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Layls from "./Containers/ProjectPage/Layls";
 import Nike from "./Containers/ProjectPage/Nike";
 import Monitor from "./Containers/ProjectPage/Monitor";
