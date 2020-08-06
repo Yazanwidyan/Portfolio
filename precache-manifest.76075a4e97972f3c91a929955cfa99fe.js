17b6f5177a93d3871a55236d9fa9aeb9ff266eed
Yazan widyan <yazanwidyan1@gmail.com>
2020-08-06 15:04:47 +0300

    Updates

---
+++ precache-manifest.76075a4e97972f3c91a929955cfa99fe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709afbd7d4ded1947ceb41ed48e5c0a6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4b01ef3f53dfa4021192",
+    "url": "/portfolio/static/css/main.ccc9755f.chunk.css"
+  },
+  {
+    "revision": "f69137029d0a3e225d43",
+    "url": "/portfolio/static/js/2.af8859a1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.af8859a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b01ef3f53dfa4021192",
+    "url": "/portfolio/static/js/main.e3ed1bd3.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "febf0bc74afe71a17522633571a094e0",
+    "url": "/portfolio/static/media/PROJECT-2.febf0bc7.png"
+  },
+  {
+    "revision": "4215e4762a8059f5034939aed0fe7ae0",
+    "url": "/portfolio/static/media/RAVE-Review-BestNikeRunningShoes-Body3.4215e476.jpg"
+  },
+  {
+    "revision": "1a1a34fdb0e03c97a79a6907a5d09899",
+    "url": "/portfolio/static/media/advantges1.1a1a34fd.png"
+  },
+  {
+    "revision": "7618c0311b2cb589a8cee0d69b2ed805",
+    "url": "/portfolio/static/media/advantges1@3x.7618c031.png"
+  },
+  {
+    "revision": "e14ca1f0d67f9ef63537e55a568dab61",
+    "url": "/portfolio/static/media/dd.e14ca1f0.png"
+  },
+  {
+    "revision": "7e439980527c4cea91c1f7795b5c21db",
+    "url": "/portfolio/static/media/rrrr.7e439980.jpg"
+  },
+  {
+    "revision": "f60efbec08fb853d65479b2cbf1c04f1",
+    "url": "/portfolio/static/media/sdsd.f60efbec.png"
+  },
+  {
+    "revision": "d939f532112a89952b2a780f5f8a1997",
+    "url": "/portfolio/static/media/taylor-swift-singer-qhd-2560x1440.d939f532.jpg"
+  }
+]);
